0c92f73591c2ca9a5bac822f38c15391b967af77
Adrien Thebo <adrien@puppetlabs.com>
2012-02-10 11:31:11 -0800

    Add manifest documentation

--- manifests/upgrade.pp
+++ manifests/upgrade.pp
@@ -1,3 +1,65 @@
+# == Class: pe::upgrade
+#
+# This class will perform the upgrade of PE to the specified version. Temporary
+# files for the installer will be placed in /opt/puppet/upgrade-${version}
+#
+# == Parameters
+#
+# [*version*]
+#
+# The version of PE to upgrade to. Must be in the form of
+# <major release>.<minor release>.<patch release>
+# For example, 2.0.2
+#
+# [*answersfile*]
+#
+# The path to a PE answers file. Defaults to the template
+# "pe/answers/${::hostname}.txt.erb"
+#
+# [*cleanup*]
+#
+# Whether to remove upgrade files after installation. Defaults to false.
+#
+# == Examples
+#
+#   # Minimal
+#   class { 'pe::upgrade':
+#     version => '2.0.2',
+#   }
+#
+#   # More customized
+#   class { 'pe::upgrade':
+#     version     => '2.0.2',
+#     answersfile => "site/answers/${fqdn}-answers.txt",
+#     cleanup     => true,
+#  }
+#
+# == Caveats
+#
+# This has only been tested for PE versions 2.0.0 and greater.
+#
+# == Authors
+#
+# Adrien Thebo <adrien@puppetlabs.com>
+#
+# == Copyright
+#
+# Copyright 2012 Puppet Labs Inc.
+#
+# == License
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 class pe::upgrade(
   $version,
   $answersfile = 'UNSET',
