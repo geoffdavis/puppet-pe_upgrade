f7d158d8a878f83b5f2505c5324593767e3fded4
Adrien Thebo <adrien@puppetlabs.com>
2012-02-10 11:25:38 -0800

    Removed unneeded params file

--- manifests/params.pp
+++
@@ -1,7 +0,0 @@
-class pe::params {
-
-  $upgrade_root        = '/opt/puppet/tmp'
-  $installer_tar_file  = "puppet-enterprise-${version}-all.tgz":
-  $upgrade_dir         = "puppet-enterprise-${version}-all":
-  $upgrader_executable = "${install_root}/${install_dir}/puppet-enterprise-upgrader"
-}
